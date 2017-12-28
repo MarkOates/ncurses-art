648e111fa92048de51ed15cc156b00bb8b69a594
Mark Oates <mark.oates@shopify.com>
2017-12-28 07:04:06 -0500

    Integrate new event types into event_queue resolver

--- src/AppController.cpp
+++ src/AppController.cpp
@@ -2,6 +2,7 @@
 
 #include <ncurses_art/Screen.h>
 #include <ncurses_art/HeaderBar.h>
+#include <ncurses_art/EventTypes.h>
 #include <ncurses.h>
 #include <stdexcept>
 #include <unistd.h>
@@ -39,6 +40,15 @@ void AppController::run_loop()
    {
       switch (getch())
       {
+      case 'j':
+         event_queue.append_event(EVENT_MOVE_CURSOR_DOWN);
+         break;
+      case 'k':
+         event_queue.append_event(EVENT_MOVE_CURSOR_UP);
+         break;
+      case 13:
+         event_queue.append_event(EVENT_CHOOSE_CURRENT_MENU_ITEM);
+         break;
       case 'q':
          event_queue.append_event(EVENT_ABORT_PROGRAM);
          break;
