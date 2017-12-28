83c2b24b595fe5a8864d184638af46517f0540fa
Mark Oates <mark.oates@shopify.com>
2017-12-28 03:02:19 -0500

    Render width/height info on HeaderBar

--- src/HeaderBar.cpp
+++ src/HeaderBar.cpp
@@ -1,6 +1,8 @@
 #include <ncurses_art/HeaderBar.h>
 
+#include <ncurses_art/Text.h>
 #include <ncurses.h>
+#include <sstream>
 
 HeaderBar::HeaderBar()
    : text("[Untitled]")
@@ -14,7 +16,16 @@ HeaderBar::~HeaderBar()
 void HeaderBar::draw()
 {
    attron(A_REVERSE);
+
+   // draw the primary header text
    mvprintw(0, 0, text.c_str());
    chgat(-1, A_REVERSE, 0, NULL);
+
+   // draw the info text
+   std::stringstream str;
+   str << "W: " << COLS << " H: " << LINES;
+   std::string width_height_text = str.str();
+   mvprintw(0, COLS - width_height_text.length() - 2, width_height_text.c_str());
+
    attroff(A_REVERSE);
 }
