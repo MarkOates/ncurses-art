9814359e4046b32a2fc1223abb9beb6e7cde4876
Mark Oates <mark.oates@shopify.com>
2017-12-28 04:34:55 -0500

    Add styles option to Text

--- include/ncurses_art/Text.h
+++ include/ncurses_art/Text.h
@@ -7,10 +7,12 @@ class Text
 private:
    std::string text;
    float x, y, align_x;
+   int styles;
 
 public:
    Text(std::string text, float x, float y, float align_x=0.0);
    ~Text();
 
+   void set_styles(int styles);
    void draw();
 };
--- src/Text.cpp
+++ src/Text.cpp
@@ -7,6 +7,7 @@ Text::Text(std::string text, float x, float y, float align_x)
    , x(x)
    , y(y)
    , align_x(align_x)
+   , styles(0)
 {
 }
 
@@ -14,9 +15,16 @@ Text::~Text()
 {
 }
 
+void Text::set_styles(int styles)
+{
+   this->styles = styles;
+}
+
 void Text::draw()
 {
    float str_width = text.length();
    float x_pos = x - (str_width * align_x);
+   attron(styles);
    mvprintw((int)y, (int)x_pos, text.c_str());
+   attroff(styles);
 }
