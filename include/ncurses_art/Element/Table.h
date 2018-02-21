c108d908bcf1f0672e5fc98447b0bb47493b9c37
Mark Oates <mark.oates@shopify.com>
2018-02-21 18:28:28 -0500

    Add Table element

---
+++ include/ncurses_art/Element/Table.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <ncurses_art/Element/ElementBase.h>
+#include <string>
+#include <vector>
+
+class Table : public ElementBase
+{
+private:
+   float x, y;
+   std::vector<std::vector<std::string>> elements;
+   int cursor_pos_x, cursor_pos_y;
+
+public:
+   Table(float x=0, float y=0, std::vector<std::vector<std::string>> elements={});
+   ~Table();
+
+   void set_x(float x);
+   void set_y(float y);
+
+   bool set_cursor_pos_x(int pos_x);
+   bool set_cursor_pos_y(int pos_y);
+   bool set_elements(std::vector<std::vector<std::string>> elements);
+
+   float get_x();
+   float get_y();
+   int get_cursor_pos_x();
+   int get_cursor_pos_y();
+   std::vector<std::vector<std::string>> get_elements();
+   int get_num_rows();
+   int get_num_cols();
+   void move_cursor_up();
+   void move_cursor_down();
+   void move_cursor_left();
+   void move_cursor_right();
+   std::string current_selection();
+
+   void draw() override;
+};
