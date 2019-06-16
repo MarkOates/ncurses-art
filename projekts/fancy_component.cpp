02039bba7616d16f39429d76e8bdd40d55144bde
Mark Oates <markoates0@gmail.com>
2019-06-16 19:53:52 -0400

    Constantize PROJECT_MENU

--- projekts/fancy_component.cpp
+++ projekts/fancy_component.cpp
@@ -5,8 +5,13 @@
 
 #include <ncurses.h>
 
+// actions
 #define REFRESH_TEXT_DISPLAY "refresh_text_display"
 #define BUILD_MENU "build_menu"
+
+// constants
+#define PROJECT_MENU "project_menu"
+
 
 std::map<char, std::string> command_mapping = {
    { 'j', MOVE_CURSOR_DOWN },
@@ -37,7 +42,7 @@ bool Projekt::process_event(std::string e)
       init_pair(3, COLOR_BLACK, 21);
       init_pair(4, 24, 22);
       init_pair(5, COLOR_MAGENTA, 23);
-      create_menu("main_menu").set_styles(COLOR_PAIR(1));
+      create_menu(PROJECT_MENU).set_styles(COLOR_PAIR(1));
       create_text("body_text", 80, 3).set_styles(COLOR_PAIR(2));
       create_text("command_list_text", 130, 20, 0.0, 1.0).set_styles(COLOR_PAIR(5));
 
@@ -50,20 +55,18 @@ bool Projekt::process_event(std::string e)
          "option 2",
          "option 3",
       };
-      Menu &menu = find_menu("main_menu");
+      Menu &menu = find_menu(PROJECT_MENU);
       menu.set_options(options);
    }
    else if (e == MOVE_CURSOR_DOWN)
    {
-      Menu &menu = find_menu("main_menu");
+      Menu &menu = find_menu(PROJECT_MENU);
       menu.move_cursor_down();
-      emit_event(REFRESH_TEXT_DISPLAY);
    }
    else if (e == MOVE_CURSOR_UP)
    {
-      Menu &menu = find_menu("main_menu");
+      Menu &menu = find_menu(PROJECT_MENU);
       menu.move_cursor_up();
-      emit_event(REFRESH_TEXT_DISPLAY);
    }
    else
    {
