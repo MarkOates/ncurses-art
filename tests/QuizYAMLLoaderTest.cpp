9c4d6fa8e52091ff98cf1a068f3c599af74be260
Mark Oates <markoates0@gmail.com>
2019-06-25 22:01:47 -0400

    Write first QuizYAMLLoader test using yaml-cpp :)

--- tests/QuizYAMLLoaderTest.cpp
+++ tests/QuizYAMLLoaderTest.cpp
@@ -3,8 +3,10 @@
 
 #include <QuizYAMLLoader.hpp>
 
-TEST(QuizYAMLLoaderTest, run__returns_the_expected_response)
+TEST(QuizYAMLLoaderTest, get_type_string__returns_the_expected_value)
 {
    QuizYAMLLoader quiz_yaml_loader;
-   EXPECT_TRUE(quiz_yaml_loader.load());
+
+   YAML::Node node = YAML::Load("[1, 2, 3]");
+   EXPECT_EQ("Sequence", quiz_yaml_loader.get_type_string(node));
 }
