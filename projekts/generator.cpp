269b8cef9c2db556842cf68dd69c3fe073153cac
Mark Oates <mark.oates@shopify.com>
2019-07-28 16:26:54 -0400

    Fix "stringstream" typo

--- projekts/generator.cpp
+++ projekts/generator.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char **argv)
 {
-   std::strinstream output_message;
+   std::stringstream output_message;
    output_message << "This executable is no longer the target.  The executable has been moved to the Blast project.";
    throw std::runtime_error(output_message.str());
 }
