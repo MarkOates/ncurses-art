ce0fe73bbeef302484b03b830c5c7db6ed609a1f
Mark Oates <markoates0@gmail.com>
2019-08-24 11:56:05 -0400

    Fill out "status_fetcher" so that it outputs status on all repos

--- projekts/status_fetcher.cpp
+++ projekts/status_fetcher.cpp
@@ -7,30 +7,48 @@
 #define MOVE_CURSOR_LEFT "MOVE_CURSOR_LEFT"
 #define MOVE_CURSOR_RIGHT "MOVE_CURSOR_RIGHT"
 #define INITIALIZE_SCENE "INITIALIZE_SCENE"
+#define REFRESH_STATUSES "REFRESH_STATUSES"
+
+std::map<std::string, GithubRepoStatusFetcher> statuses = {};
+
+std::string check_it(std::string label, bool check)
+{
+   std::stringstream result;
+   result << label;
+   if (check) result << " - ✓ yes";
+   else result << " - ✗ no";
+   return result.str();
+}
 
 void initialize()
 {
-   mappings['j'] = MOVE_CURSOR_DOWN;
-   mappings['k'] = MOVE_CURSOR_UP;
-   mappings['h'] = MOVE_CURSOR_LEFT;
-   mappings['l'] = MOVE_CURSOR_RIGHT;
-
-   //std::string repo = args[0];
-   std::string repo = "ncurses-art";
-
-   GithubRepoStatusFetcher github_status_fetcher;
-
    events[INITIALIZE_SCENE] = []{
+      create_text("output");
+
+      statuses = {
+         { "ncurses-art", GithubRepoStatusFetcher("ncurses-art") },
+         { "blast",       GithubRepoStatusFetcher("blast") },
+         { "fullscore",   GithubRepoStatusFetcher("fullscore") },
+         { "beebot",      GithubRepoStatusFetcher("beebot") },
+         { "allegro_flare",      GithubRepoStatusFetcher("allegro_flare") },
+      };
    };
-   events[MOVE_CURSOR_UP] = []{
-   };
-   events[MOVE_CURSOR_DOWN] = []{
-   };
-   events[MOVE_CURSOR_LEFT] = []{
-   };
-   events[MOVE_CURSOR_RIGHT] = []{
+   events[REFRESH_STATUSES] = []{
+      Text &text = find_text("output");
+
+      std::stringstream result_text;
+      for (auto &status : statuses)
+      {
+         result_text << status.first << std::endl;
+         result_text << "  " << check_it("in sync with remote", status.second.is_the_repo_in_sync_with_remote()) << std::endl;
+         result_text << "  " << check_it("has no changed files", !status.second.has_file_changes()) << std::endl;
+         result_text << "  " << check_it("has no untracked files", !status.second.has_untracked_files()) << std::endl;
+      }
+
+      text.set_text(result_text.str());
    };
 
    emit_event(INITIALIZE_SCENE);
+   emit_event(REFRESH_STATUSES);
 }
 
