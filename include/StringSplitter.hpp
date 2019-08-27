a404f1ce880dfd19a76c2a97a1ea9fabe6f2456a
Mark Oates <markoates0@gmail.com>
2019-08-27 00:25:22 -0400

    Add "StringSplitter" YAML conversion from blast

---
+++ include/StringSplitter.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+
+#include <string>
+#include <vector>
+
+
+class StringSplitter
+{
+private:
+   std::string string;
+   char delimiter;
+
+public:
+   StringSplitter(std::string string="", char delimiter=' ');
+   ~StringSplitter();
+
+
+std::vector<std::string> split();
+};
+
+
+
