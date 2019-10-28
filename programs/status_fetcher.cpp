fc4c79baf6eac2bb4dcd35e80d7bb36ae108a162
Mark Oates <mark.oates@shopify.com>
2019-10-28 10:00:23 -0400

    Remove "lightracer", replace with "lightracer-max"

--- programs/status_fetcher.cpp
+++ programs/status_fetcher.cpp
@@ -101,7 +101,7 @@ void initialize()
             { "Adventures of Beary",  GithubRepoStatusFetcher("adventures-of-beary") },
             { "Alex Park",            GithubRepoStatusFetcher("AlexPark") },
             { "dungeon",              GithubRepoStatusFetcher("dungeon") },
-            { "lightracer",           GithubRepoStatusFetcher("lightracer") },
+            { "lightracer-max",       GithubRepoStatusFetcher("lightracer-max") },
             { "beary2d",              GithubRepoStatusFetcher("beary2d") },
             { "tilemap",              GithubRepoStatusFetcher("tilemap") },
             { "motris",               GithubRepoStatusFetcher("motris") },
