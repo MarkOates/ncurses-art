e2a78e3cccd34a700706d0da6926399468e05dd0
Mark Oates <markoates0@gmail.com>
2019-11-16 18:39:59 -0500

    Enable "lister" var in ProjectComponentLister test

--- tests/Blast/ProjectComponentListerTest.cpp
+++ tests/Blast/ProjectComponentListerTest.cpp
@@ -5,8 +5,6 @@
 
 TEST(Blast__ProjectComponentLister, can_be_created_without_arguments)
 {
-   EpicDarkFont program_runner;
-   //std::string expected_string = "Hello World!";
-   //EXPECT_EQ(expected_string, program_runner.run());
+   Blast::ProjectComponentLister lister;
 }
 
