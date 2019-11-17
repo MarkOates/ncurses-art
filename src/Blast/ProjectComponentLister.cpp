3a2905b2cc2729bdd86fd09843872ebc4a3f68d9
Mark Oates <markoates0@gmail.com>
2019-11-16 19:34:01 -0500

    Add test for expected return value in ProjectComponentLister

--- src/Blast/ProjectComponentLister.cpp
+++ src/Blast/ProjectComponentLister.cpp
@@ -1,6 +1,7 @@
 
 
 #include <Blast/ProjectComponentLister.hpp>
+#include <algorithm>
 #include <sstream>
 #include <vector>
 #include <string>
@@ -30,14 +31,14 @@ std::vector<std::string> ProjectComponentLister::components()
 {
    std::vector<std::string> result = {};
 
-   std::cerr << "WARNING: This function does NOT vaildate the passed project_root_directory "
-      << "and injects it directly into a shell command.  This needs to be fixed."
-      << std::endl;
+   //std::cerr << "WARNING: This function does NOT vaildate the passed project_root_directory "
+      //<< "and injects it directly into a shell command.  This needs to be fixed."
+      //<< std::endl;
 
    std::stringstream find_command;
    find_command << "cd " << project_root_directory << " && find quintessence -type f -name \"*.q.yml\"";
    //std::cout << "FIND_COMMAND: " << find_command.str() << std::endl;
-   ShellCommandExecutorWithCallback executor(find_command.str());
+   ShellCommandExecutorWithCallback executor(find_command.str(), ShellCommandExecutorWithCallback::simple_silent_callback);
    std::string executor_response = executor.execute();
 
    //std::cout << "EXECUTOR_RESPONSE: " << executor_response << std::endl;
@@ -50,6 +51,8 @@ std::vector<std::string> ProjectComponentLister::components()
       ProjectComponentBasenameExtractor extractor(item);
       result.push_back(extractor.identify_component_basename());
    }
+
+   std::sort(result.begin(), result.end());
 
    return result;
 
--- tests/Blast/ProjectComponentListerTest.cpp
+++ tests/Blast/ProjectComponentListerTest.cpp
@@ -8,3 +8,33 @@ TEST(Blast__ProjectComponentLister, can_be_created_without_arguments)
    Blast::ProjectComponentLister lister;
 }
 
+TEST(Blast__ProjectComponentLister, will_return_the_components_in_a_project)
+{
+   std::vector<std::string> expected_components = {
+      "ASCIICharacterRenderer",
+      "Checkbox",
+      "EpicDarkFont",
+      "GithubRepoStatusFetcher",
+      "HostnameExtractor",
+      "OrigamiMommyFontLoader",
+      "ProjectComponentBasenameExtractor",
+      "ProjectFilenameGenerator",
+      "ProjectListBuilder",
+      "Question",
+      "Quiz",
+      "QuizYAMLLoader",
+      "ShellCommandExecutorWithCallback",
+      "SmallFontLoader",
+      "StringSplitter",
+      "StringTrimmer",
+      "StringVectorIntersection",
+   };
+
+  std::string project_folder_name = "ncurses-art";
+  std::string full_folder_name = std::string("/Users/markoates/Repos/") + project_folder_name;
+  Blast::ProjectComponentLister lister(full_folder_name);
+  std::vector<std::string> actual_components = lister.components();
+
+  ASSERT_EQ(expected_components, actual_components);
+}
+
