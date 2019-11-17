cd28290d2dd39614f9c65709f20a395ce0c69c0e
Mark Oates <mark.oates@shopify.com>
2019-11-17 10:21:01 -0500

    Include fragment suffix and extension when searching for fragment files

--- src/Blast/ProjectComponentLister.cpp
+++ src/Blast/ProjectComponentLister.cpp
@@ -64,13 +64,40 @@ std::string get_component_fragment_folder_name(component_fragment_t component_fr
 }
 
 
+std::string get_component_fragment_extension(component_fragment_t component_fragment)
+{
+   switch (component_fragment)
+   {
+   case COMPONENT_FRAGMENT_TYPE_HEADER:
+      return ".hpp";
+      break;
+   case COMPONENT_FRAGMENT_TYPE_SOURCE:
+      return ".cpp";
+      break;
+   case COMPONENT_FRAGMENT_TYPE_EXAMPLE:
+      return "Example.cpp";
+      break;
+   case COMPONENT_FRAGMENT_TYPE_TEST:
+      return "Text.cpp";
+      break;
+   case COMPONENT_FRAGMENT_TYPE_QUINTESSENCE:
+      return ".q.yml";
+      break;
+   default:
+      throw std::runtime_error("get_component_fragment_folder_name error: unaccounted for component_fragment_t");
+      break;
+   }
+}
+
+
 std::vector<std::string> get_components_of_fragment_type(std::string project_root_directory, component_fragment_t component_fragment)
 {
    //component_fragment
 
    std::stringstream find_command;
    std::string fragment_folder_name = get_component_fragment_folder_name(component_fragment);
-   find_command << "cd " << project_root_directory << " && find " << fragment_folder_name << " -type f -name \"*.q.yml\"";
+   std::string fragment_extension = get_component_fragment_extension(component_fragment);
+   find_command << "cd " << project_root_directory << " && find " << fragment_folder_name << " -type f -name \"*" << fragment_extension << "\"";
    ShellCommandExecutorWithCallback executor(find_command.str(), ShellCommandExecutorWithCallback::simple_silent_callback);
    std::string executor_response = executor.execute();
    StringSplitter splitter(executor_response, '\n');
