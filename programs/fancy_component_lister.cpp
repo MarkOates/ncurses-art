0cddaaf3fccfbc49ce87641ee6e4e685f1335a4e
Mark Oates <mark.oates@shopify.com>
2019-11-17 12:55:02 -0500

    Use "COMMAND_REBUILD_CURRENT_PROJECT_IN_MENU" in fcl

--- programs/fancy_component_lister.cpp
+++ programs/fancy_component_lister.cpp
@@ -76,6 +76,8 @@ bool Projekt::process_input(char ch)
    case 'k': emit_event(MOVE_CURSOR_UP); break;
    case 'q': emit_event(EVENT_ABORT_PROGRAM); break;
    case 'y': emit_event(YANK_SELECTED_TEXT); break;
+   case 'p': emit_event(COMMAND_REBUILD_CURRENT_PROJECT_IN_MENU); break;
+   case 'a': emit_event(COMMAND_REBUILD_ALL_PROJECTS_IN_MENU); break;
    case 'c': emit_event(BEEBOT_SETUP_BLAST_COMPONENT_COMMAND); break;
    default: return false; break;
    }
@@ -100,7 +102,7 @@ bool Projekt::process_event(std::string e)
       create_menu("main_menu").set_styles(COLOR_PAIR(1));
       create_text("body_text", 80, 3).set_styles(COLOR_PAIR(2));
 
-      emit_event(COMMAND_REBUILD_ALL_PROJECTS_IN_MENU);
+      emit_event(COMMAND_REBUILD_CURRENT_PROJECT_IN_MENU);
    }
    if (e == YANK_SELECTED_TEXT)
    {
@@ -124,6 +126,38 @@ bool Projekt::process_event(std::string e)
    }
    else if (e == COMMAND_REBUILD_CURRENT_PROJECT_IN_MENU)
    {
+      // get the options
+      std::vector<std::string> options = { };
+      std::string current_project_folder_name = "ncurses-art";
+      std::vector<std::pair<std::string, std::string>> project_folder_names = { { { current_project_folder_name }, {} } }; // 
+
+      // extract project components
+      for (const auto &project_folder_name : project_folder_names)
+      {
+         std::string project_folder_name_string = project_folder_name.first;
+
+         std::string full_folder_name = std::string("/Users/markoates/Repos/") + project_folder_name_string;
+         Blast::ProjectComponentLister lister(full_folder_name);
+         std::vector<std::string> actual_components = lister.components();
+
+         for (auto &actual_component : actual_components)
+         {
+            std::stringstream text_to_be_displayed_in_menu_option;
+            text_to_be_displayed_in_menu_option << project_folder_name_string << " - " << actual_component;
+
+            options.push_back(text_to_be_displayed_in_menu_option.str());
+         }
+      }
+
+      std::sort(options.begin(), options.end());
+      std::vector<std::string>::iterator it = std::unique(options.begin(), options.end());
+      options.resize(std::distance(options.begin(), it));
+
+      // fill the options into the menu
+      Menu &menu = find_menu("main_menu");
+      menu.set_options(options);
+      menu.set_x(10);
+      menu.set_y(10);
    }
    else if (e == COMMAND_REBUILD_ALL_PROJECTS_IN_MENU)
    {
