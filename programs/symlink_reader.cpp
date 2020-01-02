a36a16824fbdd6c013e549552d6b5c304cd7f575
Mark Oates <mark.oates@shopify.com>
2020-01-02 12:12:08 -0500

    Add first symlink detection

---
+++ programs/symlink_reader.cpp
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <string>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+   std::string filename = "/Users/markoates/Repos/LabyrinthOfLore/include/AllegroFlare/Inventory.hpp";
+   char linkname[256];
+
+   //ssize_t readlink(const char *path, char *buf, size_t bufsiz);
+   ssize_t r;
+   r = readlink(filename.c_str(), linkname, 256);
+
+   std::cout << filename << " -> " << linkname << std::endl;
+
+   //if (r
+
+   return 0;
+}
+
+
