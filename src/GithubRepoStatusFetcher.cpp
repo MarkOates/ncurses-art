831a8b5b73ff91ea3b72e6f9ae0a70d978841e49
Mark Oates <markoates0@gmail.com>
2020-01-27 22:16:00 -0500

    Replace manual function with "Blast::ShellCommandExecutorWithCallback" in GithubRepoStatusFetcher

--- src/GithubRepoStatusFetcher.cpp
+++ src/GithubRepoStatusFetcher.cpp
@@ -236,17 +236,8 @@ return result.str();
 
 std::string GithubRepoStatusFetcher::execute_command(const char* cmd)
 {
-// TODO: This should be replaced with Blast::ShellCommandExecutor
-std::array<char, 128> buffer;
-std::string result;
-std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-if (!pipe) {
-    throw std::runtime_error("popen() failed!");
-}
-while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-    result += buffer.data();
-}
-return result;
+Blast::ShellCommandExecutorWithCallback executor(cmd);
+return executor.execute();
 
 }
 
