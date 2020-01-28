68f021c80f9a09e8300c914235bdf463591f3eb7
Mark Oates <markoates0@gmail.com>
2020-01-27 22:29:07 -0500

    Add (failing) test for "execute_command" to find path specified

--- tests/GithubRepoStatusFetcherTest.cpp
+++ tests/GithubRepoStatusFetcherTest.cpp
@@ -10,6 +10,15 @@ TEST(GithubRepoStatusFetcherTest, get_current_branch_name_command__returns_the_s
    EXPECT_EQ(expected_shell_command, fetcher.get_current_branch_name_command());
 }
 
+TEST(GithubRepoStatusFetcherTest, execute_command__is_able_to_find_the_path_specificed)
+{
+   GithubRepoStatusFetcher fetcher("blast");
+   std::string shell_command = "(cd ~/Repos/blast && git fetch && git branch | grep \\* | cut -d ' ' -f2)";
+   std::string expected_command_result = "foo";
+
+   EXPECT_EQ(expected_command_result, fetcher.execute_command(shell_command));
+}
+
 //TEST(GithubRepoStatusFetcherTest, is_current_branch_master__returns_true_if_the_current_branch_is_set_to_master)
 //{
    //GithubRepoStatusFetcher fetcher("adventures-of-beary");
