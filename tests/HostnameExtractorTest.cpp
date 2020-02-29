85525d16abe0381213897f867e5edf80f22b0b4c
Mark Oates <markoates0@gmail.com>
2020-02-29 17:10:45 -0500

    Fix disabled test (Allow multiple possible responses)

--- tests/HostnameExtractorTest.cpp
+++ tests/HostnameExtractorTest.cpp
@@ -3,6 +3,7 @@
 #include <gmock/gmock.h>
 
 using ::testing::Eq; // <<< --- this assertion matcher needs to be updated
+using ::testing::Contains; // <<< --- this assertion matcher needs to be updated
 
 #include <HostnameExtractor.hpp>
 
@@ -11,17 +12,15 @@ TEST(HostnameExtractorTest, can_be_created_without_arguments)
    HostnameExtractor hostname_extractor;
 }
 
-TEST(DISABLED_HostnameExtractorTest, get_computer_name__returns_the_expected_response)
+TEST(HostnameExtractorTest, get_computer_name__returns_the_expected_response)
 {
    HostnameExtractor hostname_extractor;
 
-   std::string expected_possible_strings = {
+   std::vector<std::string> expected_possible_strings = {
       "Marks-Mac-mini.local",
       "Mark-Oates-MacBook-Pro.local",
    };
    std::string actual_computer_name = hostname_extractor.get_computer_name();
 
-   // vvv --- this assertion needs to be updated to ensure the actual matches one of the possible
-   //          v actual, v expected
-   EXPECT_THAT("foo", Eq("foo"));
+   EXPECT_THAT(expected_possible_strings, Contains(actual_computer_name));
 }
