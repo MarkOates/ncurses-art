3a0adfbf5205c27ecf33e9ce19adf92d231d26a4
Mark Oates <mark.oates@shopify.com>
2020-03-02 12:21:14 -0500

    Add updated Project/SymlinkChecker generated source

--- src/Blast/Project/SymlinkChecker.cpp
+++ src/Blast/Project/SymlinkChecker.cpp
@@ -27,26 +27,24 @@ bool SymlinkChecker::is_symlink()
 {
 struct stat p_statbuf;
 
-//if (lstat(filename.c_str(), &p_statbuf) < 0)
-//{
-   //throw std::runtime_error("[Project/SymlinkChecker error:] An error occurred while attempting to is_symlink");
-//}
+if (lstat(filename.c_str(), &p_statbuf) < 0)
+{
+   throw std::runtime_error("[Project/SymlinkChecker error:] An error occurred while attempting to is_symlink");
+}
 
-//return (S_ISLNK(p_statbuf.st_mode) == 1);
-return false;
+return (S_ISLNK(p_statbuf.st_mode) == 1);
 
 }
 
 std::string SymlinkChecker::read_symlink_target()
 {
-//char buff[512];
-//ssize_t len = ::readlink(filename.c_str(), buff, sizeof(buff)-1);
-//if (len != -1) {
-   //buff[len] = '\0';
-   //return std::string(buff);
-//}
-//throw std::runtime_error("an error occurred when trying to read_symlink_target in Blast/Project/SymlinkChecker");
-return "";
+char buff[512];
+ssize_t len = ::readlink(filename.c_str(), buff, sizeof(buff)-1);
+if (len != -1) {
+   buff[len] = '\0';
+   return std::string(buff);
+}
+throw std::runtime_error("an error occurred when trying to read_symlink_target in Blast/Project/SymlinkChecker");
 
 }
 } // namespace Project
