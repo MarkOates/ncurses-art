8b020ffd0c4ab6a46255ce7f15d242ac660db2ae
Mark Oates <markoates0@gmail.com>
2020-04-18 15:55:37 -0400

    Add rudimentary "pull_build" program

---
+++ programs/pull_build.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <allegro5/allegro.h>
+
+#include <ShellCommandExecutorWithCallback.hpp>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+   if (argc != 2)
+   {
+      std::cout << "Usage: pull_build \"command to execute\"";
+      return 1;
+   }
+
+   std::string command = argv[1];
+
+   while(true)
+   {
+      ShellCommandExecutorWithCallback executor(command);
+      executor.execute();
+      std::cout << "==== Command finished ====" << std::endl;
+      sleep(5);
+   }
+}
