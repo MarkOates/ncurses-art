9918fbe6acd45d2bb972e9af1dcefa53b8636853
Mark Oates <markoates0@gmail.com>
2020-04-29 18:09:29 -0400

    Symlink Blast/DirectoryExistenceChecker

---
+++ include/Blast/DirectoryExistenceChecker.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+
+#include <string>
+
+
+namespace Blast
+{
+   class DirectoryExistenceChecker
+   {
+   private:
+      std::string directory_name;
+
+   public:
+      DirectoryExistenceChecker(std::string directory_name="");
+      ~DirectoryExistenceChecker();
+
+
+   bool exists();
+   };
+}
+
+
+
---
+++ src/Blast/DirectoryExistenceChecker.cpp
@@ -0,0 +1,30 @@
+
+
+#include <Blast/DirectoryExistenceChecker.hpp>
+#include <sys/stat.h>
+
+
+namespace Blast
+{
+
+
+DirectoryExistenceChecker::DirectoryExistenceChecker(std::string directory_name)
+   : directory_name(directory_name)
+{
+}
+
+
+DirectoryExistenceChecker::~DirectoryExistenceChecker()
+{
+}
+
+
+bool DirectoryExistenceChecker::exists()
+{
+struct stat info;
+return stat(directory_name.c_str(), &info) == 0 && S_ISDIR(info.st_mode);
+
+}
+} // namespace Blast
+
+
