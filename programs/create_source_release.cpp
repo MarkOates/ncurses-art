9806bfc6550fdf249c93a97e4e1d4a7b1a00f808
Mark Oates <markoates0@gmail.com>
2020-05-14 08:09:13 -0400

    Rename "create_release" program to "create_source_release"

--- programs/create_source_release.cpp
+++ programs/create_source_release.cpp
@@ -7,8 +7,8 @@ int main(int argc, char **argv)
    // call from whatever project directory you're in, it will add the release to the "~/Desktop/Release/" folder.
 
    std::string destination_directory="/Users/markoates/Desktop/Release";
-   Blast::Project::SourceReleaseBuilder release_builder(destination_directory);
-   release_builder.generate_source_release();
+   Blast::Project::SourceReleaseBuilder source_release_builder(destination_directory);
+   source_release_builder.generate_source_release();
 
    return 0;
 }
